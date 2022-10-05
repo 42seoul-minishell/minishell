cfb757dd8abefd785b91f6ba9543342dd322f1f4
Mingyu Kim <soil0201@gmail.com>
2022-10-05 12:53:32 +0900

    Init Makefile and testfiles

--- src/internal/internal.c
+++ src/internal/internal.c
@@ -1,31 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   internal.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mingkim <mingkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/05 10:58:16 by mingkim           #+#    #+#             */
-/*   Updated: 2022/10/05 12:45:25 by mingkim          ###   ########.fr       */
+/*   Created: 2022/10/05 11:50:29 by mingkim           #+#    #+#             */
+/*   Updated: 2022/10/05 11:51:10 by mingkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int	main(int ac, char **av, char **env)
+void	print_hello(void)
 {
-	int	len;
-	int	len2;
-
-	len = 0;
-	while (len < ac)
-	{
-		printf("av[%d] = %s\n", len, av[len]);
-		len++;
-	}
-	len2 = 0;
-	while (env[len2])
-		len2++;
-	len = 0;
-	return (0);
+	printf("hello world!\n");
 }
--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,31 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mingkim <mingkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/05 10:58:16 by mingkim           #+#    #+#             */
-/*   Updated: 2022/10/05 12:45:25 by mingkim          ###   ########.fr       */
+/*   Created: 2022/10/05 11:42:46 by mingkim           #+#    #+#             */
+/*   Updated: 2022/10/05 11:46:53 by mingkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int	main(int ac, char **av, char **env)
+size_t	ft_strlen(const char *str)
 {
-	int	len;
-	int	len2;
+	size_t	i;
 
-	len = 0;
-	while (len < ac)
-	{
-		printf("av[%d] = %s\n", len, av[len]);
-		len++;
-	}
-	len2 = 0;
-	while (env[len2])
-		len2++;
-	len = 0;
-	return (0);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
