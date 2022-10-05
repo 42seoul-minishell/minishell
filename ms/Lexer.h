b6a470b597817d8aa2acc5fdf5e3c9f63795a632
Bonghak Lee <bolee@c2r6s6.42seoul.kr>
2022-10-05 14:36:17 +0900

    이전 작업물 ms 리포지토리에 추가했습니다

---
+++ ms/Lexer.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Lexer.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bolee <bolee@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/21 21:00:20 by bolee             #+#    #+#             */
+/*   Updated: 2022/09/21 23:40:16 by bolee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEXER_H
+// 어휘 분석기
+# define LEXER_H
+
+# include "Token.h"
+
+/* Tokens structure */
+typedef struct s_tokens
+{
+	t_token			*token;
+	struct s_tokens	*next;
+}	t_tokens;
+
+/* in Lexer.c */
+/* 문자열을 입력 받아 토큰을 생성하는 함수 */ 
+int			make_tokens(t_tokens **tokens, char *expr);
+
+/* about tokens structure in Lexer.c */
+t_tokens	*tokens_new(t_token *token);
+t_tokens	*tokens_last(t_tokens *tokens);
+void		tokens_add_back(t_tokens **token, t_tokens *new);
+
+#endif
---
+++ ms/Utils/Standard.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Standard.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bolee <bolee@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/21 21:15:13 by bolee             #+#    #+#             */
+/*   Updated: 2022/09/21 21:15:14 by bolee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STANDARD_H
+# define STANDARD_H
+
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+
+char	**ft_split(char const *s, char c);
+char	*ft_strchr(const char *s, int c);
+
+#endif
