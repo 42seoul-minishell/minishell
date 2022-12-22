a8ea9f5a36871b65c1bf142c8d8f8ce7204f7a98
LEEBONGHAK <dlqhdgkr456@gmail.com>
2022-12-22 19:34:57 +0900

    [bintree]
    바이너리 트리 구조 변경에 따른 에러 발생
    바이너리 트리 구조가 완성되기 전까지 주석처리 후 추후 수정 예정

--- src/internal/executor/command.c
+++ src/internal/executor/command.c
@@ -12,39 +12,39 @@
 
 #include "minishell.h"
 
-static void _separate_cmd(t_bintree_node *node)
-{
-	char **split;
+// static void _separate_cmd(t_bintree_node *node)
+// {
+// 	char **split;
 
-	if (!node)
-		return;
-	split = ft_split(node->token->value, ' ');
-	if (!split)
-		exit(1);
-	if (ft_strncmp(split[0], "echo", ft_strlen(split[0])) == 0)
-		builtin_echo("echo", 0, 0);
-	else if (ft_strncmp(split[0], "cd", ft_strlen(split[0])) == 0)
-		builtin_cd(split[1]);
-	else if (ft_strncmp(split[0], "pwd", ft_strlen(split[0])) == 0)
-		builtin_pwd();
-	else if (ft_strncmp(split[0], "export", ft_strlen(split[0])) == 0)
-		builtin_export(split[1], split[2]);
-	else if (ft_strncmp(split[0], "unset", ft_strlen(split[0])) == 0)
-		builtin_unset(split[1]);
-	else if (ft_strncmp(split[0], "env", ft_strlen(split[0])) == 0)
-		builtin_env();
-	else if (ft_strncmp(split[0], "exit", ft_strlen(split[0])) == 0)
-		builtin_exit(1);
-	else
-		not_found_error(split[0]);
-	free_dp((void **) split);
-}
+// 	if (!node)
+// 		return;
+// 	split = ft_split(node->token->value, ' ', 0);
+// 	if (!split)
+// 		exit(1);
+// 	if (ft_strncmp(split[0], "echo", ft_strlen(split[0])) == 0)
+// 		builtin_echo("echo", 0, 0);
+// 	else if (ft_strncmp(split[0], "cd", ft_strlen(split[0])) == 0)
+// 		builtin_cd(split[1]);
+// 	else if (ft_strncmp(split[0], "pwd", ft_strlen(split[0])) == 0)
+// 		builtin_pwd();
+// 	else if (ft_strncmp(split[0], "export", ft_strlen(split[0])) == 0)
+// 		builtin_export(split[1], split[2]);
+// 	else if (ft_strncmp(split[0], "unset", ft_strlen(split[0])) == 0)
+// 		builtin_unset(split[1]);
+// 	else if (ft_strncmp(split[0], "env", ft_strlen(split[0])) == 0)
+// 		builtin_env();
+// 	else if (ft_strncmp(split[0], "exit", ft_strlen(split[0])) == 0)
+// 		builtin_exit(1);
+// 	else
+// 		not_found_error(split[0]);
+// 	free_dp((void **) split);
+// }
 
 void execute_command(t_bintree_node *node)
 {
 	if (node->lc)
 		executor(node->lc);
-	_separate_cmd(node);
+	// _separate_cmd(node);
 	if (node->rc)
 		executor(node->rc);
 }
