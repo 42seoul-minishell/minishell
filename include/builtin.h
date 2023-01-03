18909597028e54981cdc7c436dfc48be5d34ee83
LEEBONGHAK <dlqhdgkr456@gmail.com>
2023-01-03 16:58:30 +0900

    [main]
    builtin.h 수정

--- include/builtin.h
+++ include/builtin.h
@@ -20,7 +20,7 @@
 int		builtin_cd(t_list *lst, int out_fd);
 
 /* echo.c */
-int		builtin_echo(t_list *lst);
+int		builtin_echo(t_list *lst, int out_fd);
 
 /* env.c */
 void	builtin_env(int out_fd);
@@ -29,7 +29,7 @@ void	builtin_env(int out_fd);
 void	builtin_exit(t_list *lst);
 
 /* export.c */
-int		builtin_export(t_list *lst);
+int		builtin_export(t_list *lst, int out_fd);
 
 /* pwd.c */
 int		builtin_pwd(int out_fd);
